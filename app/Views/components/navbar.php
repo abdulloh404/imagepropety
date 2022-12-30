31a2fcba6ef17c2304f6ae946273b39d2a95ea2c
Abdulloh404 <adlforgithub@gmail.com>
2022-12-30 14:48:00 +0700

    UPDATE : Gitignore

--- app/Views/components/navbar.php
+++ app/Views/components/navbar.php
@@ -63,7 +63,7 @@
             <div class="offcanvas-body d-xl-flex align-items-center">
                 <ul class="navbar-nav mx-auto pe-3">
                     <li class="nav-item">
-                        <a class="nav-link active-1" aria-current="page" href="index.php">หน้าแรก</a>
+                        <a class="nav-link active-1" aria-current="page" href="<?php echo front_link(1) ?>">หน้าแรก</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link active-2" href="<?php echo front_link(2) ?>">โครงการของเรา</a>
