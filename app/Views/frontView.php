c664c74e3db329a20028edca5b2be3959d799ef7
Theratat Somprasert <107108213+Theratat@users.noreply.github.com>
2023-01-05 14:02:17 +0700

    Update frontView.php

--- app/Views/frontView.php
+++ app/Views/frontView.php
@@ -2,10 +2,10 @@
 <html lang="en">
 
 <?php echo view('components/header') ?>
-<link rel="stylesheet" href="page/assets/css/index.css">
+<!-- <link rel="stylesheet" href="page/assets/css/index.css">
 <link rel="stylesheet" href="page/assets/css/about.css">
 <link rel="stylesheet" href="page/assets/css/blog.css">
-<link rel="stylesheet" href="page/assets/css/contact-us.css">
+<link rel="stylesheet" href="page/assets/css/contact-us.css"> -->
 
 
 <body>
