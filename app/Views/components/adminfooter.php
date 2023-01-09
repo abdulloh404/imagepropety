b60fee3243e93ee70c6db9f98611088d51335c6f
Theratat Somprasert <107108213+Theratat@users.noreply.github.com>
2023-01-09 10:26:59 +0700

    ++

---
+++ app/Views/components/adminfooter.php
@@ -0,0 +1,21 @@
+<style>
+    .footer {
+        position: relative;
+        right: 0;
+        bottom: 0;
+        left: 0;
+        padding: 5px;
+        margin-top: 30px;
+        box-shadow: rgba(67, 71, 85, 0.27) 0px 0px 0.25em, rgba(90, 125, 188, 0.05) 0px 0.25em 1em;
+        text-align: center;
+    }
+</style>
+
+<footer>
+    <div class="footer">
+        <div class="text-center mt-2">Copyright © 2023 <strong>Image Property Management Co,.Ltd</strong> All rights reserved.</div>
+        <div class="text-center mb-3">บริษัท อิมเมจ พร็อพเพอตี้ แมเนจเม้นท์ จำกัด
+            79 หมู่ 8 ถนนมิตรภาพ-หนองคาย ต.หมื่นไวย อ.เมืองนครราชสีมา จ.นครราชสีมา 30000
+        </div>
+    </div>
+</footer>
---
+++ app/Views/components/adminheader.php
@@ -0,0 +1,30 @@
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>ImageProperty</title>
+
+    <link rel="icon" type="image/x-icon" href="./assets/img/logo.png">
+
+    <link rel="stylesheet" href="./assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="./assets/css/style.css">
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" integrity="sha512-1ycn6IcaQQ40/MKBW2W4Rhis/DbILU74C1vSrLJxCq57o941Ym01SwNsOMqvEBFlcgUa6xLiPY/NS5R+E6ztJQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+    <script src="./assets/js/bootstrap.bundle.min.js"></script>
+
+    <!--Css DataTable-->
+    <link rel="stylesheet" type="text/css" href="./assets/css/jquery.dataTables.min.css"/>
+    <link rel="stylesheet" type="text/css" href="./assets/css/rowReorder.dataTables.min.css"/>
+    <link rel="stylesheet" type="text/css" href="./assets/css/responsive.dataTables.min.css"/>
+    
+    <script src="./assets/js/jquery.min.js"></script>
+    
+    <!--plug-in DataTable -->
+    <script src="./assets/js/jquery.dataTables.min.js"></script>
+    <script src="./assets/js/dataTables.responsive.min.js"></script>
+    <script src="./assets/js/dataTables.rowReorder.min.js"></script>
+    
+
+</head>
